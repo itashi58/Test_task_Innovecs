afc30477f64b4994725869d587c9a6c3e84bc0e1
Danil <itashi5858@users.noreply.github.com>
2021-12-19 15:01:08 +0200

    created initial design

--- app/src/main/java/com/example/test_task_innovecs/MainActivity.kt
+++ app/src/main/java/com/example/test_task_innovecs/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.test_task_innovecs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.lifecycle.ViewModelProvider
+import com.example.test_task_innovecs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val items = resources.getStringArray(R.array.cities)
+        val adapter = ArrayAdapter(baseContext, R.layout.city_list_item, items)
+        binding.cityChooserAuto.setAdapter(adapter)
+
+        val viewModel =  ViewModelProvider(this).get(MainViewModel::class.java)
+        
     }
 }
---
+++ app/src/main/java/com/example/test_task_innovecs/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.test_task_innovecs
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(): ViewModel() {
+
+}
